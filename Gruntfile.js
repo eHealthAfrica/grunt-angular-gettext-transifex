0c612a0739cd8052cf27780797b0f14d3954d3ff
Robin Mehner <robin@coding-robin.de>
2015-06-11 17:21:47 +0200

    feat(all): Initial release

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+/*
+ * grunt-angular-gettext-transifex
+ * https://github.com/eHealthAfrica/grunt-angular-gettext-transifex
+ *
+ * Copyright (c) 2015 Robin Mehner
+ * Licensed under the Apache, 2.0 licenses.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.loadTasks('tasks');
+};
---
+++ tasks/angular-gettext-transifex.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var isCI = function() {
+  return ['CI', 'TRAVIS', 'CONTINUOUS_INTEGRATION'].filter(function(variable) {
+    return process.env[variable] === 'true';
+  }).length > 0;
+}
+
+module.exports = function(grunt) {
+  // we need to jump this hoop so we can load our dependencies
+  // and use their tasks
+  var cwd = process.cwd();
+  process.chdir(__dirname + '/..');
+  grunt.loadNpmTasks('grunt-tx');
+  grunt.loadNpmTasks('grunt-angular-gettext');
+  process.chdir(cwd);
+
+  grunt.registerTask('ng-gettext-transifex-upload', function() {
+    if (!isCI()) {
+      grunt.fatal('Not allowed in a non-CI environment. Run with CI=true to override this.');
+    }
+
+    grunt.task.run('nggettext_extract', 'tx:upload');
+  });
+
+  grunt.registerTask('ng-gettext-transifex-download', [
+    'tx:download',
+    'nggettext_compile'
+  ]);
+}
